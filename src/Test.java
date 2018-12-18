15a288aee4e695189cff8ba6101f3f0ce02be1fc
Mildrloll <42993951+Mildrloll@users.noreply.github.com>
2018-12-18 13:24:20 +0100

    bla

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
 
 public class Test {
-    private static MediaManager mm=new MediaManager();
+    private static MediaManager mm = new MediaManager();
+
     // Denne metode indeholder kode, som kan vise applikationens funktionalitet (ikke unit test)
     public static void test() {
         System.out.println("TV2 Media Manager");
@@ -9,7 +10,7 @@ public class Test {
         m1.setName("Julemanden");
         m1.setFileName("julemand.jpg");
         System.out.println(m1.toString());
-        ArrayList filer=mm.readFolder("media/");
+        ArrayList filer = mm.readFolder("media/");
         for (int i = 0; i < filer.size(); i++) {
             System.out.println(filer.get(i));
         }
