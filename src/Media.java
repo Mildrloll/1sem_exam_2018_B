ab5e2acc7b37e5650dd24635e918115ffffbb54d
Mildrloll <42993951+Mildrloll@users.noreply.github.com>
2018-12-18 12:41:55 +0100

    Slette udnødvedig ting.

--- src/Media.java
+++ src/Media.java
@@ -9,16 +9,6 @@ public class Media {
     private String name;//This crates a string with the name name that can only be used in this class.
     private Date created = new Date();//This crates a Date with the name created that can only be used in this class.
     private String fileName;//This crates a string with the name fileName that can only be used in this class.
-    private String filType;
-    private int durationMinutes;
-    private int durationSeconds;
-    private String resolution;
-    private String photographer;
-    private int widthInPixels;
-    private int heightInPixels;
-    private String author;
-    private String articleText;
-    private String relatedImage;
 
     public Media() {
         //This Media method crates a ID from the MediaID class for the assetId and takes the date off today for the created.
@@ -57,85 +47,6 @@ public class Media {
 
     public void setFileName(String fileName) {
         this.fileName = fileName;
-    }
-
-    public String getfilType() {
-        return filType;
-    }
-
-    public void setFilType(String filType) {
-        this.filType = filType;
-    }
-
-    public int getDurationMinutes() {
-        return durationMinutes;
-    }
-
-    public void setDurationMinutes(int durationMinutes) {
-        this.durationMinutes = durationMinutes;
-    }
-
-    public int getDurationSeconds() {
-        return durationSeconds;
-    }
-
-    public void setDurationSeconds(int durationSeconds) {
-        this.durationSeconds = durationSeconds;
-    }
-    public String getResolution() {
-        return resolution;
-    }
-
-    public void setResolution(String resolution) {
-        this.resolution = resolution;
-    }
-
-    public String getPhotographer() {
-        return photographer;
-    }
-
-    public void setPhotographer(String photographer) {
-        this.photographer = photographer;
-    }
-
-    public int getWidthInPixels() {
-        return widthInPixels;
-    }
-
-    public void setWidthInPixels(int widthInPixels) {
-        this.widthInPixels = widthInPixels;
-    }
-
-    public int getHeightInPixels() {
-        return heightInPixels;
-    }
-
-    public void setHeightInPixels(int heightInPixels) {
-        this.heightInPixels = heightInPixels;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getArticleText() {
-        return articleText;
-    }
-
-    public void setArticleText(String articleText) {
-        this.articleText = articleText;
-    }
-
-    public String getRelatedImage() {
-        return relatedImage;
-    }
-
-    public void setRelatedImage(String relatedImage) {
-        this.relatedImage = relatedImage;
     }
 
     @Override
